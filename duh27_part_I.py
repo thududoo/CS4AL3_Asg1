7ea5097422967d6a24c7ab14ab39b3f3cb5e17fc
Forbesii Du <thududoo@gmail.com>
2024-09-20 16:17:47 -0400

    Finished algo for part 1

--- duh27_part_I.py
+++ duh27_part_I.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as an
 import random
-import time
 
 CSV_PATH = "./gdp-vs-happiness.csv"
-RATE = 1e-7
-MAX_STEP = 1000000
-THRESHOLD = 1e-16
+RATE = 1e-2
+MAX_STEP = 1000
+THRESHOLD = 1e-6
 
 class LRM:
     def __init__(self, csv_path, y_col, x_col):
@@ -18,6 +17,7 @@ class LRM:
         self.load_csv(csv_path, y_col, x_col)
         self.input = np.array(self.xs, dtype=float)
         self.target = np.array(self.ys, dtype=float)
+        self.input_nrm = (self.input - np.mean(self.input)) / np.std(self.input)
         self.set_size = float(len(self.input))
 
     def load_csv(self, csv_path, y_col, x_col):
@@ -35,40 +35,37 @@ class LRM:
         X = np.column_stack((np.ones(len(self.input)), self.input))
         Y = np.column_stack(self.target).T
         self.beta_ols = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+        xs = np.linspace(min(self.input), max(self.input), num=2)
+        ys = self.beta_ols[1] * xs + self.beta_ols[0]
+        plt.plot(xs, ys, "purple")
     
     def gradient_loss_func(self, slope, intrc):
-        y_prid = slope * self.input + intrc
-        d_intrc = sum(
-                self.input * (self.target - y_prid)
-            ) * -2 / self.set_size
-        d_slope = sum(
-            (self.target - y_prid)
-            ) * -2 / self.set_size
+        y_pred = slope * self.input_nrm + intrc
+        d_slope = np.sum(self.input_nrm * (self.target - y_pred)) * -2 / self.set_size
+        d_intrc = np.sum(self.target - y_pred) * -2 / self.set_size
+        
         return d_slope, d_intrc
 
     def train_gd(self):
         slope = random.random()
-        # intrc = random.random()
-        # intrc = 0
-        # slope = self.beta_ols[1]
-        intrc = float(self.beta_ols[0])
-        for _ in range(MAX_STEP):
+        intrc = random.random()
+        epoch = 0
 
+        for _ in range(MAX_STEP):
             slope_pd, intrc_pd = self.gradient_loss_func(slope, intrc)
-            # print(f"{slope=}, {intrc=}")
-            # print(f"{slope_pd=}, {intrc_pd=}\n")
             if abs(slope_pd) <= THRESHOLD or abs(intrc_pd) <= THRESHOLD:
                 print("break on threshold")
                 break
             slope = slope - slope_pd * RATE
-            intrc = intrc - intrc_pd * RATE * 5e-4
+            intrc = intrc - intrc_pd * RATE
+            epoch += 1
 
         print(f"{slope=}, {intrc=}")
         print(f"{slope_pd=}, {intrc_pd=}\n")
         xs = np.linspace(min(self.input), max(self.input), num=2)
         ys = slope * xs + intrc
         
-        plt.plot(xs, ys)
+        plt.plot(xs, ys, "red")
         self.plot()
 
 

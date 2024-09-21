f70545d75304afcb89811afa6d74d42e29980ac8
Forbesii Du <thududoo@gmail.com>
2024-09-21 18:42:28 -0400

    Part I finished

--- duh27_part_I.py
+++ duh27_part_I.py
@@ -6,81 +6,105 @@ import seaborn as an
 import random
 
 CSV_PATH = "./gdp-vs-happiness.csv"
-RATE = 1e-2
-MAX_STEP = 10000
-THRESHOLD = 1e-6
+RATE = 1e-3
+MAX_STEP = 300
 
 class LRM:
-    def __init__(self, csv_path, y_col, x_col):
+    def __init__(self, csv_path):
         self.ys = []
         self.xs = []
-        self.load_csv(csv_path, y_col, x_col)
+        self.load_csv(csv_path)
         self.input = np.array(self.xs, dtype=float)
         self.target = np.array(self.ys, dtype=float)
-        self.input_nrm = (self.input - np.mean(self.input)) / np.std(self.input)
+        self.input = (self.input - np.mean(self.input)) / np.std(self.input)
+        self.target = (self.target - np.mean(self.target)) / np.std(self.target)
         self.set_size = float(len(self.input))
+        self.X = np.column_stack((np.ones(len(self.input)), self.input))
+        self.Y = np.column_stack(self.target).T
 
-    def load_csv(self, csv_path, y_col, x_col):
-        csv_file =  open(csv_path, "r")
-        csv_file.readline()
-        line = csv_file.readline()
-        while line:
-            line = line.split(",")
-            if line[x_col] and line[y_col]:
-                self.ys.append(line[y_col])
-                self.xs.append(line[x_col])
-            line = csv_file.readline()
+    def load_csv(self, csv_path):
+        # import data
+        data = pd.read_csv(csv_path)
+        #drop columns that will not be used
+        by_year = (data[data['Year']==2018]).drop(columns=["Continent","Population (historical estimates)","Code"])
+        # remove missing values from columns 
+        df = by_year[(by_year['Cantril ladder score'].notna()) & (by_year['GDP per capita, PPP (constant 2017 international $)']).notna()]
+        for row in df.iterrows():
+            if row[1]['Cantril ladder score']>4.5:
+                self.ys.append(row[1]['Cantril ladder score'])
+                self.xs.append(row[1]['GDP per capita, PPP (constant 2017 international $)'])
 
     def train_ols(self):
-        X = np.column_stack((np.ones(len(self.input)), self.input))
-        Y = np.column_stack(self.target).T
+        X = self.X
+        Y = self.Y
         self.beta_ols = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
-        xs = np.linspace(min(self.input), max(self.input), num=2)
-        ys = self.beta_ols[1] * xs + self.beta_ols[0]
-        plt.plot(xs, ys, "purple")
-    
-    def gradient_loss_func(self, slope, intrc):
-        y_pred = slope * self.input_nrm + intrc
-        d_slope = np.sum(self.input_nrm * (self.target - y_pred)) * -2 / self.set_size
-        d_intrc = np.sum(self.target - y_pred) * -2 / self.set_size
-        
-        return d_slope, d_intrc
 
     def train_gd(self):
-        slope = random.random()
-        intrc = random.random()
-        epoch = 0
+        X = self.X
+        Y = self.Y
+        n = self.set_size
+        self.beta_gd = {}
+        for alpha in (1, 5):
+            beta = np.random.randn(2, 1)
+            for epoch in range(MAX_STEP):
+                gradients =  2 / n * (X.T).dot(X.dot(beta) - Y)
+                beta = beta - alpha * RATE * gradients
+                if (epoch + 1) % (MAX_STEP / 3) == 0:
+                    self.beta_gd[(alpha * RATE, epoch + 1)] = beta
 
-        for _ in range(MAX_STEP):
-            slope_pd, intrc_pd = self.gradient_loss_func(slope, intrc)
-            if abs(slope_pd) <= THRESHOLD or abs(intrc_pd) <= THRESHOLD:
-                print("break on threshold")
-                break
-            slope = slope - slope_pd * RATE
-            intrc = intrc - intrc_pd * RATE
-            # epoch
+    def loss(self, beta):
+        y_pred = self.X.dot(beta)
+        residuals = self.Y - y_pred
+        return np.sum(residuals ** 2)
 
-        print(f"{slope=}, {intrc=}")
-        print(f"{slope_pd=}, {intrc_pd=}\n")
-        xs = np.linspace(min(self.input), max(self.input), num=2)
-        ys = slope * xs + intrc
-        
-        plt.plot(xs, ys, "red")
-        self.plot()
+    def find_best_beta(self):
+        min_RSE = float("inf")
+        for re, beta in self.beta_gd.items():
+            RSE = self.loss(beta)
+            if RSE < min_RSE:
+                min_RSE = RSE
+                best_beta = beta
+                best_rate_epoch = re
+        return best_rate_epoch, best_beta
 
-
-            
-            
-    
     def plot(self):
-        plt.scatter(self.input, self.target)
+        xs = np.linspace(min(self.input), max(self.input), num=2)
+        plt.figure()
+        plt.xlabel("GDP")
+        plt.ylabel("Happiness")
+        plt.scatter(self.input, self.target, s=5)
+        print(f"Gradient Descent Method:")
+        for (rate, epoch), beta in self.beta_gd.items():
+            ys = beta[1] * xs + beta[0]
+            plt.plot(xs, ys,
+                     label=f"GD@{rate=} & {epoch=}"
+                     )
+            print(f"\tbeta = [{float(beta[0][0]):.5E}, {float(beta[1][0]):.5E}]\t@ {rate = } & {epoch = }")
+        plt.legend()
+        print()
+        plt.figure()
+        plt.xlabel("GDP")
+        plt.ylabel("Happiness")
+        plt.scatter(self.input, self.target, s=5)
+        ys = self.beta_ols[1] * xs + self.beta_ols[0]
+        plt.plot(xs, ys, label="OLS", linewidth=3)
+        (rate, epoch), beta = self.find_best_beta()
+        ys = beta[1] * xs + beta[0]
+        plt.plot(xs, ys,
+                 label=f"Best GD@{rate=} & {epoch=}"
+                 )
+        print(f"Ordinary Least Squares Method:")
+        print(f"\tbeta = [{float(self.beta_ols[0][0]):.5E}, {float(self.beta_ols[1][0]):.5E}]\t@ {rate = } & {epoch = }")
+        print(f"Best from Gradient Descent Method:")
+        print(f"\tbeta = [{float(beta[0][0]):.5E}, {float(beta[1][0]):.5E}]\t@ {rate = } & {epoch = }")
+        plt.legend()
         plt.show()
 
 
 
 
 if __name__ == "__main__":
-    model = LRM(csv_path=CSV_PATH, y_col=3, x_col=4)
+    model = LRM(csv_path=CSV_PATH)
     model.train_ols()
-    print(model.beta_ols)
     model.train_gd()
+    model.plot()
